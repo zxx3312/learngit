2c9c624e04d20ce8d01c00fb1840f2e20a38a69e
Cynthia <327362307@qq.com>
2024-05-10 09:45:09 +0800

    remove test.java

--- test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(4);
-    }
-}
